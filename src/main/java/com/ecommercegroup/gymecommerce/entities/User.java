8705f386702a8aa06ff645db79ae5f7f0740aa31
Gustavo Fernandes <gustavofdev@gmail.com>
2025-03-05 19:32:21 -0300

    feature: starting with Spring Security, new config SecurityFilterChain

--- src/main/java/com/ecommercegroup/gymecommerce/entities/User.java
+++ src/main/java/com/ecommercegroup/gymecommerce/entities/User.java
@@ -10,8 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Table(name = "tb_user")
 @Entity

e1655bb03eb96a1f98bf51812fcbe8a1f9208ce7
Gustavo Fernandes <gustavofdev@gmail.com>
2025-03-05 23:26:13 -0300

    feature: UserDetailsService using InMemoryUserDetailsManager for tests, using PasswordEncoder BCryptPasswordEncoder

--- src/main/java/com/ecommercegroup/gymecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/ecommercegroup/gymecommerce/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,4 +30,29 @@ public class SecurityConfiguration {
 				.build();
 		
 	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder(10);
+	}
+	
+	@Bean
+	UserDetailsService userDetailService (PasswordEncoder encoder) {
+		UserDetails user1 = User.builder()
+				.username("usuario")
+				.password(encoder.encode("123456"))
+				.roles("USER")
+				.build();
+		
+		UserDetails user2 = User.builder()
+				.username("admin")
+				.password(encoder.encode("admin"))
+				.roles("ADMIN")
+				.build();
+				
+		return new InMemoryUserDetailsManager(user1, user2);
+		
+	}
+	
 }
